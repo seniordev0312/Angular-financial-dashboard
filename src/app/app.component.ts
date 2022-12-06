f3661b504f257f736806461121ee8f397d5752bb
seniordev0312 <devsenior0312@gmail.com>
2022-12-06 09:10:06 +0200

    feature/607-Integrate-with-Identity-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { authService } from './auth/auth.service';
 import { EntitiesControlComponent } from './pages/entities/entities-control/components/entities-control/entities-control.component';
 import { EntitiesViewerComponent } from './pages/entities/entities-viewer/components/entities-viewer/entities-viewer.component';
 import { TranslationService } from './shared/services/translation.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Insurance Power House';
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
@@ -30,8 +30,18 @@ export class AppComponent {
 
   constructor(
     private dialog: MatDialog,
-    translationService: TranslationService
+    translationService: TranslationService,
+    private authService: authService
   ) {
     translationService.setDefaultLanguage();
   }
+
+  ngOnInit() {
+    this.authService
+      .checkAuth()
+      .subscribe((isAuthenticated) =>
+        console.log('app authenticated', isAuthenticated)
+      );
+  }
+
 }
