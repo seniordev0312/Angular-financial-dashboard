70e63eea06b651d54e77bbaf67287f0a6e8936fd
seniordev0312 <devsenior0312@gmail.com>
2022-12-15 15:04:52 +0200

    feature/581-implement-general-strucutre

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://dev.api.entity.aperatureuk.com/api',
   identityServerURL: 'https://dev.api.identity.aperatureuk.com/api',
+  signalRHub: 'http://20.241.255.241'
 };
 
 
