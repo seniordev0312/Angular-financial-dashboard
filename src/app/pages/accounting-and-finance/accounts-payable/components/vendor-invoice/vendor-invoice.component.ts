5776e9663859d385bd32ecf99f7ff74b0dcbf90f
seniordev0312 <devsenior0312@gmail.com>
2023-02-01 21:21:18 +0800

    vendor-invoice dialog done

--- src/app/pages/accounting-and-finance/accounts-payable/components/vendor-invoice/vendor-invoice.component.ts
+++ src/app/pages/accounting-and-finance/accounts-payable/components/vendor-invoice/vendor-invoice.component.ts
@@ -1,16 +1,128 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { WidgetTableComponent } from '@root/shared/components/widget-table/widget-table.component';
+import { PayableModel } from '../../model/payable.model';
+import { TableColumn } from '@root/shared/models/table/table-column.model';
+import { TableColumnFilterDataType } from '@root/shared/models/table/enum/table-column-filter-data-type.enum';
+import { TableConfiguration } from '@root/shared/models/table/table-configuration.model';
+import { TableSettings } from '@root/shared/models/table/table-settings.model';
 
 @Component({
   selector: 'app-vendor-invoice',
   templateUrl: './vendor-invoice.component.html',
   styleUrls: ['./vendor-invoice.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VendorInvoiceComponent implements OnInit {
+  @ViewChild(WidgetTableComponent)
+  table: WidgetTableComponent<PayableModel>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  tableColumns: TableColumn[] = [
+    {
+      translationKey: 'ein',
+      property: 'ein',
+      type: 'text',
+      svgIcon: '',
+      cssClasses: () => '',
+      dataCssClasses: () => 'underline text-accent',
+      enableSort: true,
+      hasFilter: true,
+      visible: true,
+      displayInFilterList: false,
+      hasToolTip: false,
+      showText: true,
+      filter: {
+        filterType: TableColumnFilterDataType.Text,
+      },
+    },
+    {
+      translationKey: 'name',
+      property: 'name',
+      type: 'text',
+      cssClasses: () => '',
+      dataCssClasses: () => '',
+      enableSort: true,
+      hasFilter: true,
+      visible: true,
+      displayInFilterList: true,
+      hasToolTip: false,
+      showText: true,
+      filter: {
+        filterType: TableColumnFilterDataType.Text,
+      },
+    },
+    {
+      translationKey: 'currency',
+      property: 'currency',
+      type: 'text',
+      cssClasses: () => '',
+      dataCssClasses: () => '',
+      enableSort: true,
+      hasFilter: true,
+      visible: true,
+      displayInFilterList: true,
+      hasToolTip: false,
+      showText: true,
+      filter: {
+        filterType: TableColumnFilterDataType.Text,
+      },
+    },
+    {
+      translationKey: 'amount',
+      property: 'amount',
+      type: 'number',
+      cssClasses: () => '',
+      dataCssClasses: () => '',
+      enableSort: true,
+      hasFilter: true,
+      visible: true,
+      displayInFilterList: true,
+      hasToolTip: false,
+      showText: true,
+      filter: {
+        filterType: TableColumnFilterDataType.Text,
+      },
+    },
+    {
+      translationKey: 'amountnotdue',
+      property: 'amountnotdue',
+      type: 'number',
+      cssClasses: () => '',
+      dataCssClasses: () => '',
+      enableSort: true,
+      hasFilter: true,
+      visible: true,
+      displayInFilterList: true,
+      hasToolTip: false,
+      showText: true,
+      filter: {
+        filterType: TableColumnFilterDataType.Text,
+      },
+    },
+  ];
+
+  pageSize = 15;
+
+  tableSettings = new TableSettings({
+    actionsMode: 'inline',
+    pageSize: this.pageSize,
+    isLocalPaging: true,
+    isRowSelectable: true,
+  });
+
+  tableConfiguration: TableConfiguration<PayableModel> = {
+    tableRowsActionsList: [],
+    columns: this.tableColumns,
+    data: [],
+    dataCount: 0,
+    settings: this.tableSettings,
+  };
 }
