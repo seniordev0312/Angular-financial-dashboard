74a42b8c5c7b2cc138a52552731c78e4ac4866e9
seniordev0312 <devsenior0312@gmail.com>
2023-02-15 20:54:34 +0800

    add account receivable page to the list in the left side bar.

--- src/app/pages/accounting-and-finance/accounts-receivable/accounts-receivable.module.ts
+++ src/app/pages/accounting-and-finance/accounts-receivable/accounts-receivable.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountsReceivableComponent } from './components/accounts-receivable/accounts-receivable.component';
+import { Route, RouterModule } from '@angular/router';
+import { ApplicationRoutes } from '@root/shared/settings/common.settings';
+import { Permission } from '@root/shared/models/enums/permissions.enum';
+import { AutoLoginAllRoutesGuard } from 'angular-auth-oidc-client';
+import { SecurityGuard } from '@root/shared/guards/security.guard';
 
-
+const routes: Route[] = [
+  {
+    path: ApplicationRoutes.Empty,
+    component: AccountsReceivableComponent,
+    data: {
+      permission: Permission.CanAccessAccountsPayable,
+    },
+    canActivate: [AutoLoginAllRoutesGuard, SecurityGuard],
+  },
+];
 
 @NgModule({
-  declarations: [
-    AccountsReceivableComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AccountsReceivableComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class AccountsReceivableModule { }
+export class AccountsReceivableModule {}
