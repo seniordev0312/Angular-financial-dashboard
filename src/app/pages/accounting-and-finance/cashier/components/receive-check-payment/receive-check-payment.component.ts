89a400a0a31aef4691b60d597e20068ff568dd61
seniordev0312 <devsenior0312@gmail.com>
2023-03-14 14:43:56 +0000

    finish the ticket

--- src/app/pages/accounting-and-finance/cashier/components/receive-check-payment/receive-check-payment.component.ts
+++ src/app/pages/accounting-and-finance/cashier/components/receive-check-payment/receive-check-payment.component.ts
@@ -4,13 +4,16 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   selector: 'app-receive-check-payment',
   templateUrl: './receive-check-payment.component.html',
   styleUrls: ['./receive-check-payment.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReceiveCheckPaymentComponent implements OnInit {
+  processSection: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  process() {
+    this.processSection = true;
   }
-
 }
