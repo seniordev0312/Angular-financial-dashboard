1aa1a9147d4f5ab162e32d612fa19aa9647e2d54
seniordev0312 <devsenior0312@gmail.com>
2023-03-17 10:57:32 +0000

    finish ticket

--- src/app/pages/accounting-and-finance/cashier/components/person-make-payment-body/person-make-payment-body.component.ts
+++ src/app/pages/accounting-and-finance/cashier/components/person-make-payment-body/person-make-payment-body.component.ts
@@ -1,16 +1,35 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  Input,
+} from '@angular/core';
+import { BaseListItem } from '@root/shared/models/base-list-item.model';
 
 @Component({
   selector: 'app-person-make-payment-body',
   templateUrl: './person-make-payment-body.component.html',
   styleUrls: ['./person-make-payment-body.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonMakePaymentBodyComponent implements OnInit {
+  @Input() totalPrice: number;
+  currentDate: string;
+  moneyOptions: BaseListItem[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.currentDate = Date.now().toString();
+    this.moneyOptions = [
+      {
+        id: 0,
+        value: 'USD',
+      },
+      {
+        id: 1,
+        value: 'LBP',
+      },
+    ];
   }
-
 }
