728d791f42846f8ccc64f9fe8b8ef5e6a7795b28
seniordev0312 <devsenior0312@gmail.com>
2023-03-20 09:33:23 +0200

    fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@ export const environment = {
   identityAPIServerURL: window[<any>"env"][<any>"identityAPIServerURL"] || "default",
   customerServiceSignalRHub: window[<any>"env"][<any>"customerServiceSignalRHub"] || "default",
   customerService: window[<any>"env"][<any>"customerService"] || "default",
-  identityServerURL: window[<any>"env"][<any>"identityServerURL"] || "default",
+  identityServerURL: window[<any>"env"][<any>"identityServerURL"] as unknown as string || '',
   systemSetupApiUrl: window[<any>"env"][<any>"systemSetupApiUrl"] || "default",
   accountingUrl: window[<any>"env"][<any>"accountingUrl"] || "default",
   entityApiUrl: window[<any>"env"][<any>"entityApiUrl"] || "default",
-  postLogoutRedirectUri: window[<any>"env"][<any>"postLogoutRedirectUri"] || "default",
+  postLogoutRedirectUri: window[<any>"env"][<any>"postLogoutRedirectUri"] as unknown as string || '',
 };
