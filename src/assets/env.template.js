55cdd7cf168d4372d89162ab2ec152a082095dcc
seniordev0312 <devsenior0312@gmail.com>
2023-03-20 09:27:35 +0200

    fixes

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -1,12 +1,13 @@
 (function (window) {
-    window["env"] = window["env"] || {};
-    window["env"]["production"] = false;
-    window["env"]["identityAPIServerURL"] = "${IDENTITY_API_SERVER_URL}";
-    window["env"]["customerServiceSignalRHub"] = "${CUSTOMER_SERVICE_SIGNAL_R_HUB}";
-    window["env"]["customerService"] = "${CUSTOMER_SERVICE}";
-    window["env"]["identityServerURL"] = "${IDENTITY_SERVER_URL}";
-    window["env"]["systemSetupApiUrl"] = "${SYSTEM_SETUP_API_URL}";
-    window["env"]["accountingUrl"] = "${ACCOUNTING_URL}";
-    window["env"]["entityApiUrl"] = "${ENTITY_API_URL}";
-    window["env"]["postLogoutRedirectUri"] = "${POST_LOGOUT_REDIRECT_URI}";
+  window["env"] = window["env"] || {};
+  window["env"]["production"] = "${PRODUCTION}";
+  window["env"]["identityAPIServerURL"] = "${IDENTITY_API_SERVER_URL}";
+  window["env"]["customerServiceSignalRHub"] =
+    "${CUSTOMER_SERVICE_SIGNAL_R_HUB}";
+  window["env"]["customerService"] = "${CUSTOMER_SERVICE}";
+  window["env"]["identityServerURL"] = "${IDENTITY_SERVER_URL}";
+  window["env"]["systemSetupApiUrl"] = "${SYSTEM_SETUP_API_URL}";
+  window["env"]["accountingUrl"] = "${ACCOUNTING_URL}";
+  window["env"]["entityApiUrl"] = "${ENTITY_API_URL}";
+  window["env"]["postLogoutRedirectUri"] = "${POST_LOGOUT_REDIRECT_URI}";
 })(this);
