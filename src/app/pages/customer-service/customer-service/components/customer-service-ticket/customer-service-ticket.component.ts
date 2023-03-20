a22082c5c5d28b283d7d0ae82dc2c59405fae660
seniordev0312 <devsenior0312@gmail.com>
2023-03-20 09:59:34 +0200

    #871 - Finalized emergency flow

--- src/app/pages/customer-service/customer-service/components/customer-service-ticket/customer-service-ticket.component.ts
+++ src/app/pages/customer-service/customer-service/components/customer-service-ticket/customer-service-ticket.component.ts
@@ -50,7 +50,6 @@ export class CustomerServiceTicketComponent implements OnInit {
   otherFlowFlag: boolean = false;
   complaintFlowFlag: boolean = false;
   pendingCardFlag: boolean = false;
-  isActionConfirmed: boolean = false;
   isShowAppField = false;
   isLoading = false;
   canShowCalculator = false;
@@ -282,9 +281,7 @@ export class CustomerServiceTicketComponent implements OnInit {
     this.locationSectionFlag = true;
   }
 
-  displayEmergencyInitateSection(emergencyInitiateItem?: any) {
-    this.isActionConfirmed = false;
-
+  displayEmergencyInitateSection() {
     this.subscription = this.customerCardService
       .getEmergencyInitiateItems(this.choosedButtons.emergencyType)
       .subscribe((data: any) => {
@@ -294,36 +291,35 @@ export class CustomerServiceTicketComponent implements OnInit {
         this.ref.detectChanges();
       });
 
-    if (emergencyInitiateItem) {
+    this.emergencyInitialSectionFlag = true;
+  }
+
+  onSelectEmergencyItem(emergencyInitiateItem: any) {
+    if (
+      emergencyInitiateItem &&
+      !this.choosedButtons.initiate.includes(emergencyInitiateItem.id)
+    ) {
       let location = this.location.value;
 
       this.dialog
         .open(ConfirmEmergencyActionComponent, {
           width: '25%',
-          height: '30%',
-          data: { emergencyAction: emergencyInitiateItem, location: location },
+          height: '200px',
+          data: {
+            emergencyAction: emergencyInitiateItem,
+            location: location,
+            choosedButtons: this.choosedButtons.initiate,
+          },
         })
         .afterClosed()
         .subscribe((result) => {
-          this.choosedButtons.initiate.push(result.emergencyAction);
-          // this.isActionConfirmed = result.isActionConfirmed;
-
-    console.log(this.choosedButtons.initiate);
-          // this.selectActionButtons(emergencyInitiateItem);
+          if (result) {
+            this.choosedButtons.initiate.push(result.emergencyAction);
+            this.ref.detectChanges();
+          }
         });
     }
-
-    this.emergencyInitialSectionFlag = true;
-    console.log(this.choosedButtons.initiate);
   }
-
-  // selectActionButtons(emergencyInitiateItem?: any) {
-  //   if (this.isActionConfirmed) {
-  //     this.choosedButtons.initiate.push(emergencyInitiateItem.id);
-  //   }
-  //   console.log(this.isActionConfirmed);
-  //   console.log(this.choosedButtons.initiate);
-  // }
 
   RequestDraftPolicy() {
     // this.isLoading = true;
