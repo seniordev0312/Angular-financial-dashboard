acffd3d819f1a78c4ae4958665292ddeeed01ad6
seniordev0312 <devsenior0312@gmail.com>
2023-03-30 10:36:03 +0300

    #816 - Chat html message handling

--- src/app/pages/customer-service/customer-service/customer-service.module.ts
+++ src/app/pages/customer-service/customer-service/customer-service.module.ts
@@ -72,7 +72,7 @@ const routes: Route[] = [
     HistoryListComponent,
     EditHistoryComponent,
     CustomerServiceTicketComponent,
-    PendingInformaitonCardComponent,
+    PendingInformaitonCardComponent
   ],
   imports: [
     CommonModule,
